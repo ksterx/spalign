6136e1c62bb0ff478936f5a04b7afa30add05cc8
ksterx <kostonerx@gmail.com>
2025-08-16 12:14:06 +0900

    update

--- scripts/generation/upload_scenario.py
+++ scripts/generation/upload_scenario.py
@@ -1,8 +1,16 @@
-from datasets import load_dataset
+if __name__ == "__main__":
+    import argparse
 
-# ds = load_dataset("json", data_files="scenarios/*.json", split="train")
-ds = load_dataset("json", data_files="scenarios/scenario_*.json", split="train").map(
-    lambda x: {"character_list": x["metadata"]["character_list"]}
-)
+    from datasets import load_dataset
 
-ds.push_to_hub("Spiral-AI/Synthesized-Scenario-20250812", private=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--date", type=str, default="250812")
+    args = parser.parse_args()
+
+    ds = load_dataset(
+        "json", data_files="scenarios/scenario_*.json", split="train"
+    ).map(lambda x: {"character_list": x["metadata"]["character_list"]})
+
+    print(ds)
+
+    ds.push_to_hub(f"Spiral-AI/Synthesized-Scenario-{args.date}", private=True)
